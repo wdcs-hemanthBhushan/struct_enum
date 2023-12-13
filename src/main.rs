21701ed6feafd7495d1ccad1cf6dc2dffc868958
hemanthbhushanCZ <hemant.bhushan@codezeros.com>
2023-12-13 15:23:43 +0530

    refg

--- src/main.rs
+++ src/main.rs
@@ -2,38 +2,54 @@
 struct Weapon {
     name: String,
     power: Powers,
+    strenth: i32,
 }
 #[derive(Debug)]
 enum Powers {
-    easy,
-    med,
-    hard,
-    ultra,
+    Easy,
+    Med,
+    Hard,
+    Ultra,
 }
 
 impl Weapon {
     fn default() -> Self {
         Self {
             name: "knife".to_owned(),
-            power: Powers::easy,
+            power: Powers::Easy,
+            strenth: 23,
         }
     }
 
-    fn check(&self) -> (&String, &Powers) {
-        (&self.name, &self.power)
+    fn check_strnth(&mut self) -> &i32 {
+        self.strenth += 26;
+        &self.strenth
+    }
+
+    fn claim_weapon(&self) {
+        match &self.strenth {
+            0..=23 => println!("stil in the biggner level"),
+            24..=100 => println!("your eligible to claim the Ultra weapon"),
+            _ => println!("your powerfullll"),
+        }
     }
 
     fn upgrade(&mut self) {
-        self.name = "Gun".to_owned();
-        self.power = Powers::hard;
+        match &self.strenth {
+            24..=100 => {
+                self.name = "Gun".to_owned();
+                self.power = Powers::Ultra;
+            }
+            _ => println!("ur not eligible to upgrade"),
+        }
     }
 
     fn know_your_weapon(&self) {
         match &self.power {
-            Powers::easy => println!(" this is bigginer level {:?}", Powers::easy),
-            Powers::med => println!(" this is med level {:?}", Powers::med),
-            Powers::hard => println!(" this is hard level {:?}", Powers::hard),
-            Powers::ultra => println!(" this is ultra level {:?}", Powers::ultra),
+            Powers::Easy => println!(" this is bigginer level {:?}", Powers::Easy),
+            Powers::Med => println!(" this is Med level {:?}", Powers::Med),
+            Powers::Hard => println!(" this is Hard level {:?}", Powers::Hard),
+            Powers::Ultra => println!(" this is Ultra level {:?}", Powers::Ultra),
         }
     }
 }
@@ -41,7 +57,13 @@ impl Weapon {
 fn main() {
     let mut u1 = Weapon::default();
 
-    println!("{:?}", u1.know_your_weapon());
+    u1.know_your_weapon();
+
+    u1.check_strnth();
+    u1.claim_weapon();
     u1.upgrade();
-    println!("{:?}", u1.know_your_weapon());
+    u1.know_your_weapon();
+    u1.check_strnth();
+    u1.claim_weapon();
+    u1.know_your_weapon();
 }
